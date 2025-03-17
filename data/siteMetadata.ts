21eeaf6be96c2166cc03d636237e039d44d60703
erickleite338 <erickleite338@gmail.com>
2025-03-17 17:02:41 +0000

    feat(siteMetadata): Update `author` field value to 'Super Erick'

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -6,7 +6,7 @@ interface ExtendedPlinyConfig extends PlinyConfig {
 
 const siteMetadata: ExtendedPlinyConfig = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Super Erick',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
