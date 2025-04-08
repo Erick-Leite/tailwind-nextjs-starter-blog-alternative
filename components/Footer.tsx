8eec5abd974bcd3d416604db35883da3aa8bbf62
erickleite338 <erickleite338@gmail.com>
2025-04-08 13:58:28 +0000

    refactor(Footer): Add `ICON_SIZE` variable

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,21 +3,23 @@ import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
+  const ICON_SIZE = 6
+
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-          <SocialIcon kind="bluesky" href={siteMetadata.bluesky} size={6} />
-          <SocialIcon kind="x" href={siteMetadata.x} size={6} />
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
-          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
-          <SocialIcon kind="medium" href={siteMetadata.medium} size={6} />
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={ICON_SIZE} />
+          <SocialIcon kind="github" href={siteMetadata.github} size={ICON_SIZE} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={ICON_SIZE} />
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={ICON_SIZE} />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={ICON_SIZE} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={ICON_SIZE} />
+          <SocialIcon kind="bluesky" href={siteMetadata.bluesky} size={ICON_SIZE} />
+          <SocialIcon kind="x" href={siteMetadata.x} size={ICON_SIZE} />
+          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={ICON_SIZE} />
+          <SocialIcon kind="threads" href={siteMetadata.threads} size={ICON_SIZE} />
+          <SocialIcon kind="medium" href={siteMetadata.medium} size={ICON_SIZE} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
